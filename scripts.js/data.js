9fd772e92909acf071984c33140129c9f3c798a2
raposamillar <lisa.raposamillar@gmail.com>
2024-12-17 09:27:48 -0500

    add remaining narration categories and tweak existing object properties

--- scripts.js/data.js
+++ scripts.js/data.js
@@ -1,38 +1,44 @@
 export const concertos = [
   {
     Title: 'Academic Festival Rag for Piano and Wind Ensemble',
-    Type: 'Concerto for Piano and Wind Ensemble',
+    Category: 'Concertos',
+    Forces: 'Piano and Wind Ensemble',
     dateOfComposition: '2012',
     Duration: '10.5 minutes',
     Score: '61 pp.',
-    partOrParts: '163 pp',
-    Note: 'Includes two-piano version for rehearsal',
+    partOrParts: '163 pp.',
+    programNoteAvailable: 'Yes',
     sheetMusicFormat: 'PDF',
     recordingFormat: 'mp3; Live Performance',
+    URL: 'https://www.christopherkies.com/store/academic-festival-rag-for-piano-and-wind-ensemble',
     catalogueNo: '001'
   },
   {
     Title: 'Academic Festival Rag for Two Pianos',
-    Type: 'Concerto Arranged for Two Pianos',
+    Category: 'Concertos',
+    Forces: 'Two Pianos',
     dateOfComposition: '2012',
     Duration: '10.5 minutes',
     Score: '32 pp.',
     partOrParts: 'n/a',
-    Note: 'n/a',
+    programNoteAvailable: 'Yes',
     sheetMusicFormat: 'PDF',
     recordingFormat: 'n/a',
+    URL: 'https://www.christopherkies.com/store/academic-festival-rag-for-two-pianos',
     catalogueNo: '002'
   },
   {
     Title: 'Introduction and Ragtime for Clarinet in B-flat and Piano',
-    Type: 'Concertino for Clarinet in B-flat and Piano',
+    Category: 'Concertos',
+    Forces: 'Clarinet in B-flat and Piano',
     dateOfComposition: '2012',
     Duration: '9.25 minutes',
     Score: '19 pp.',
     partOrParts: '7 pp.',
-    Note: 'n/a',
+    programNoteAvailable: 'Yes',
     sheetMusicFormat: 'PDF',
     recordingFormat: 'mp3; Live Performance',
+    URL: 'https://www.christopherkies.com/store/introduction-and-rag-for-clarinet-and-orchestra-arranged-for-clarinet-and-piano',
     catalogueNo: '004'
   }
 ]
@@ -40,14 +46,16 @@ export const concertos = [
 export const orchestral = [
   {
     Title: 'Franklin Portrait: Preamble for Orchestra',
-    Type: 'Orchestral',
+    Category: 'Orchestra',
+    Forces: 'Orchestra',
     dateOfComposition: '2006',
     Duration: '5 minutes',
     Score: '22 pp.',
-    partOrParts: '57 pp',
-    Note: 'Composed on the occasion of the Tercentenary of Benjamin Franklin’s birth for Franklin High School (Franklin, Massachusetts)',
+    partOrParts: '57 pp.',
+    programNoteAvailable: 'Yes',
     sheetMusicFormat: 'PDF',
     recordingFormat: 'mp3; Live Performance',
+    URL: 'https://www.christopherkies.com/store/franklin-portrait-preamble-for-orchestra',
     catalogueNo: '007'
   }
 ]
@@ -55,26 +63,176 @@ export const orchestral = [
 export const orchestraNarration = [
   {
     Title: 'Le Tombeau de Saint-Saëns',
-    Type: 'Orchestra with Narration',
+    Category: 'Orchestra with Narration',
+    Forces: 'Orchestra and Narrator',
     dateOfComposition: '2003',
     Duration: '23.5 minutes',
     Score: '115 pp.',
     partOrParts: '336 pp.',
-    Note: 'n/a',
+    programNoteAvailable: 'Yes',
     sheetMusicFormat: 'PDF',
     recordingFormat: 'mp3; Live Performance',
+    URL: 'https://www.christopherkies.com/store/le-tombeau-de-saint-saens-for-orchestra-and-narrator',
     catalogueNo: '010'
   },
   {
     Title: 'Little Red Riding Hood (or Ladle Rat Rotten Hut)',
-    Type: 'Orchestra with Narration',
+    Category: 'Orchestra with Narration',
+    Forces: 'Orchestra and Narrator',
     dateOfComposition: '2008',
     Duration: '13 minutes',
     Score: '45 pp.',
-    partOrParts: '88 pp',
-    Note: 'n/a',
+    partOrParts: '88 pp.',
+    programNoteAvailable: 'Yes',
     sheetMusicFormat: 'PDF',
     recordingFormat: 'mp3; Live Performance',
+    URL: 'https://www.christopherkies.com/store/little-red-riding-hood-or-ladle-rat-rotten-hut-for-orchestra-and-narrator',
     catalogueNo: '011'
+  }
+]
+
+export const concertBandNarration = [
+  {
+    Title: 'Little Red Riding Hood or Ladle Rat Rotten Hut for Concert Band and Narrator',
+    Category: 'Concert Band with Narration',
+    Forces: 'Concert Band and Narrator',
+    dateOfComposition: '2009',
+    Duration: '12.5 minutes',
+    Score: '51 pp.',
+    partOrParts: '89 pp.',
+    programNoteAvailable: 'Yes',
+    sheetMusicFormat: 'PDF',
+    recordingFormat: 'mp3; Live Performance',
+    URL: 'https://www.christopherkies.com/store/littleredridinghoodforconcertbandnarrator',
+    catalogueNo: '014'
+  }
+]
+
+export const chamberEnsemblesNarration = [
+  {
+    Title: 'Le Tombeau de Saint-Saëns for Woodwind Quintet and Narrator',
+    Category: 'Chamber Ensembles with Narration',
+    Forces: 'Woodwind Quintet and Narrator',
+    dateOfComposition: '2013',
+    Duration: '18.5 minutes',
+    Score: '43 pp.',
+    partOrParts: '91 pp.',
+    programNoteAvailable: 'Yes',
+    sheetMusicFormat: 'PDF',
+    recordingFormat: 'n/a',
+    URL: 'https://www.christopherkies.com/store/le-tombeau-de-saint-saens-for-woodwind-quintet-and-narrator',
+    catalogueNo: '020'
+  },
+  {
+    Title: 'Le Tombeau de Saint-Saëns for Marimba, Clarinet in B flat, Piano and Narrator',
+    Category: 'Chamber Ensembles with Narration',
+    Forces: 'Marimba, Clarinet in B-flat, Piano, and Narrator',
+    dateOfComposition: '2020',
+    Duration: '18.5 minutes',
+    Score: '36 pp.',
+    partOrParts: '29 pp.',
+    programNoteAvailable: 'Yes',
+    sheetMusicFormat: 'PDF',
+    recordingFormat: 'mp3; MIDI',
+    URL: 'https://www.christopherkies.com/store/le-tombeau-de-saint-saens-for-marimba-clarinet-in-b-flat-piano-and-narrator',
+    catalogueNo: '020b'
+  },
+  {
+    Title: 'Le Tombeau de Saint-Saëns for Piano Four-Hands and Narrator',
+    Category: 'Chamber Ensembles with Narration',
+    Forces: 'Piano Four-Hands and Narrator',
+    dateOfComposition: '2020',
+    Duration: '18.5 minutes',
+    Score: '42 pp.',
+    partOrParts: 'n/a',
+    programNoteAvailable: 'Yes',
+    sheetMusicFormat: 'PDF',
+    recordingFormat: 'mp3; MIDI excerpts for nos. 1, 4, 10, and 13',
+    URL: 'https://www.christopherkies.com/store/letombeaudesaintsaenspianofourhandsandnarrator',
+    catalogueNo: '020c'
+  },
+  {
+    Title: 'Le Tombeau de Saint-Saëns for Brass Quintet and Narrator',
+    Category: 'Chamber Ensembles with Narration',
+    Forces: 'Brass Quintet and Narrator',
+    dateOfComposition: '2002, rev. 2009',
+    Duration: '18.5 minutes',
+    Score: '46 pp.',
+    partOrParts: '83 pp.',
+    programNoteAvailable: 'Yes',
+    sheetMusicFormat: 'PDF',
+    recordingFormat: 'mp3; MIDI',
+    URL: 'https://www.christopherkies.com/store/le-tombeau-de-saint-saens-for-brass-quintet-and-narrator',
+    catalogueNo: '021'
+  },
+  {
+    Title: 'Little Red Riding Hood (or Ladle Rat Rotten Hut) for Piano Four-Hands and Narrator',
+    Category: 'Chamber Ensembles with Narration',
+    Forces: 'Piano Four-Hands and Narrator',
+    dateOfComposition: '2007; arr. for piano four-hands in 2012',
+    Duration: '12.5 minutes',
+    Score: '27 pp.',
+    partOrParts: 'n/a',
+    programNoteAvailable: 'Yes',
+    sheetMusicFormat: 'PDF',
+    recordingFormat: 'mp3; MIDI (no narrator)',
+    URL: 'https://www.christopherkies.com/store/little-red-riding-hood-or-ladle-rat-rotten-hut-for-piano-four-hands-and-narrator',
+    catalogueNo: '022 / 053'
+  },
+  {
+    Title: 'Little Red Riding Hood (or Ladle Rat Rotten Hut) for Oboe, Bassoon, Piano, and Narrator',
+    Category: 'Chamber Ensembles with Narration',
+    Forces: 'Oboe, Bassoon, Piano, and Narrator',
+    dateOfComposition: '2007',
+    Duration: '12.5 minutes',
+    Score: '25 pp.',
+    partOrParts: '10 pp.',
+    programNoteAvailable: 'Yes',
+    sheetMusicFormat: 'PDF',
+    recordingFormat: 'mp3; Recording',
+    URL: 'https://www.christopherkies.com/store/little-red-riding-hood-or-ladle-rat-rotten-hut-for-oboe-bassoon-and-piano',
+    catalogueNo: '023'
+  },
+  {
+    Title: 'Little Red Riding Hood (or Ladle Rat Rotten Hut) for Flute, Bassoon, Piano and Narrator',
+    Category: 'Chamber Ensembles with Narration',
+    Forces: 'Flute, Bassoon, Piano, and Narrator',
+    dateOfComposition: '2007',
+    Duration: '12.5 minutes',
+    Score: '25 pp.',
+    partOrParts: '8 pp.',
+    programNoteAvailable: 'Yes',
+    sheetMusicFormat: 'PDF',
+    recordingFormat: 'n/a',
+    URL: 'https://www.christopherkies.com/store/little-red-riding-hood-for-flute-bassoon-piano-and-narrator',
+    catalogueNo: '024'
+  },
+  {
+    Title: 'Little Red Riding Hood (or Ladle Rat Rotten Hut) for Clarinet in B Flat, Cello, Piano, and Narrator',
+    Category: 'Chamber Ensembles with Narration',
+    Forces: 'Clarinet in B-flat, Cello, Piano, and Narrator',
+    dateOfComposition: '2007',
+    Duration: '12.5 minutes',
+    Score: '27 pp.',
+    partOrParts: '11 pp.',
+    programNoteAvailable: 'Yes',
+    sheetMusicFormat: 'PDF',
+    recordingFormat: 'mp3; Live Performance',
+    URL: 'https://www.christopherkies.com/store/little-red-riding-hood-or-ladle-rat-rotten-hut',
+    catalogueNo: '026'
+  },
+  {
+    Title: 'Old Business for Piano, Contrabass, Drum Set, and Narrator',
+    Category: 'Chamber Ensembles with Narration',
+    Forces: 'Piano, Contrabass, Drum Set, and Narrator',
+    dateOfComposition: '2012',
+    Duration: '11 minutes',
+    Score: '68 pp.',
+    partOrParts: '16 pp.',
+    programNoteAvailable: 'Short Description',
+    sheetMusicFormat: 'PDF',
+    recordingFormat: 'mp3; Live Performance',
+    URL: 'https://www.christopherkies.com/store/old-business',
+    catalogueNo: '027'
   }
 ]
--- scripts.js/index.js
+++ scripts.js/index.js
@@ -1,10 +1,14 @@
 import { concertos } from './data.js'
 import { orchestral } from './data.js'
 import { orchestraNarration } from './data.js'
+import { concertBandNarration } from './data.js'
+import { chamberEnsemblesNarration } from './data.js'
 
 const concertosContainer = document.getElementById('concertos-container')
 const orchestraContainer = document.getElementById('orchestra-container')
 const orchestraNarrContainer = document.getElementById('orchestra-narr-container')
+const concertBandNarrContainer = document.getElementById('concert-band-narr-container')
+const chamberEnsemblesNarrContainer = document.getElementById('chamber-ensemble-narr-container')
 
 let concertoHTML = ''
 
@@ -12,14 +16,16 @@ concertos.forEach(concerto => {
     concertoHTML += `
     <div class="concerto">
       <h3>${concerto.Title}</h3>
-      <p><strong>Type: </strong>${concerto.Type}</p>
+      <p><strong>Category: </strong>${concerto.Category}</p>
+      <p><strong>Forces: </strong>${concerto.Forces}</p>
       <p><strong>Date of Composition: </strong>${concerto.dateOfComposition}</p>
       <p><strong>Duration: </strong>${concerto.Duration}</p>
       <p><strong>Score: </strong>${concerto.Score}</p>
       <p><strong>Part(s): </strong>${concerto.partOrParts}</p>
-      <p><strong>Note: </strong>${concerto.Note}</p>
+      <p><strong>programNoteAvailable: </strong>${concerto.programNoteAvailable}</p>
       <p><strong>Sheet Music Format: </strong>${concerto.sheetMusicFormat}</p>
       <p><strong>Recording Format: </strong>${concerto.recordingFormat}</p>
+      <p><strong>URL: </strong><a href="${concerto.URL}" target="_blank">${concerto.URL}</a></p>
       <p><strong>Catalogue Number: </strong>${concerto.catalogueNo}</p>
     </div>
     <hr>
@@ -33,14 +39,16 @@ concertos.forEach(concerto => {
     orchestraHTML += `
     <div class="orchestra">
       <h3>${orchestra.Title}</h3>
-      <p><strong>Type: </strong>${orchestra.Type}</p>
+      <p><strong>Category: </strong>${orchestra.Category}</p>
+      <p><strong>Forces: </strong>${orchestra.Forces}</p>
       <p><strong>Date of Composition: </strong>${orchestra.dateOfComposition}</p>
       <p><strong>Duration: </strong>${orchestra.Duration}</p>
       <p><strong>Score: </strong>${orchestra.Score}</p>
       <p><strong>Part(s): </strong>${orchestra.partOrParts}</p>
-      <p><strong>Note: </strong>${orchestra.Note}</p>
+      <p><strong>programNoteAvailable: </strong>${orchestra.programNoteAvailable}</p>
       <p><strong>Sheet Music Format: </strong>${orchestra.sheetMusicFormat}</p>
       <p><strong>Recording Format: </strong>${orchestra.recordingFormat}</p>
+      <p><strong>URL: </strong><a href="${orchestra.URL}" target="_blank">${orchestra.URL}</a></p>
       <p><strong>Catalogue Number: </strong>${orchestra.catalogueNo}</p>
     </div>
     <hr>
@@ -54,18 +62,66 @@ concertos.forEach(concerto => {
     orchestraNarrHTML += `
     <div class="orchestraNarr">
       <h3>${orchestraNarr.Title}</h3>
-      <p><strong>Type: </strong>${orchestraNarr.Type}</p>
+      <p><strong>Category: </strong>${orchestraNarr.Category}</p>
+      <p><strong>Forces: </strong>${orchestraNarr.Forces}</p>
       <p><strong>Date of Composition: </strong>${orchestraNarr.dateOfComposition}</p>
       <p><strong>Duration: </strong>${orchestraNarr.Duration}</p>
       <p><strong>Score: </strong>${orchestraNarr.Score}</p>
       <p><strong>Part(s): </strong>${orchestraNarr.partOrParts}</p>
-      <p><strong>Note: </strong>${orchestraNarr.Note}</p>
+      <p><strong>programNoteAvailable: </strong>${orchestraNarr.programNoteAvailable}</p>
       <p><strong>Sheet Music Format: </strong>${orchestraNarr.sheetMusicFormat}</p>
       <p><strong>Recording Format: </strong>${orchestraNarr.recordingFormat}</p>
+     <p><strong>URL: </strong><a href="${orchestraNarr.URL}" target="_blank">${orchestraNarr.URL}</a></p>
       <p><strong>Catalogue Number: </strong>${orchestraNarr.catalogueNo}</p>
     </div>
     <hr>
   `
   })
   orchestraNarrContainer.innerHTML = orchestraNarrHTML;
+
+  let concertBandNarrHTML = ''
+
+  concertBandNarration.forEach(concertBandNarr => {
+    concertBandNarrHTML += `
+    <div class="concertBandNarr">
+      <h3>${concertBandNarr.Title}</h3>
+      <p><strong>Category: </strong>${concertBandNarr.Category}</p>
+      <p><strong>Forces: </strong>${concertBandNarr.Forces}</p>
+      <p><strong>Date of Composition: </strong>${concertBandNarr.dateOfComposition}</p>
+      <p><strong>Duration: </strong>${concertBandNarr.Duration}</p>
+      <p><strong>Score: </strong>${concertBandNarr.Score}</p>
+      <p><strong>Part(s): </strong>${concertBandNarr.partOrParts}</p>
+      <p><strong>programNoteAvailable: </strong>${concertBandNarr.programNoteAvailable}</p>
+      <p><strong>Sheet Music Format: </strong>${concertBandNarr.sheetMusicFormat}</p>
+      <p><strong>Recording Format: </strong>${concertBandNarr.recordingFormat}</p>
+      <p><strong>URL: </strong><a href="${concertBandNarr.URL}" target="_blank">${concertBandNarr.URL}</a></p>
+      <p><strong>Catalogue Number: </strong>${concertBandNarr.catalogueNo}</p>
+    </div>
+    <hr>
+  `
+  })
+  concertBandNarrContainer.innerHTML = concertBandNarrHTML;
+
+  let chamberEnsemblesNarrHTML = ''
+
+  chamberEnsemblesNarration.forEach(chamberEnsemblesNarr => {
+    chamberEnsemblesNarrHTML += `
+    <div class="chamberEnsemblesNarr">
+      <h3>${chamberEnsemblesNarr.Title}</h3>
+      <p><strong>Category: </strong>${chamberEnsemblesNarr.Category}</p>
+      <p><strong>Forces: </strong>${chamberEnsemblesNarr.Forces}</p>
+      <p><strong>Date of Composition: </strong>${chamberEnsemblesNarr.dateOfComposition}</p>
+      <p><strong>Duration: </strong>${chamberEnsemblesNarr.Duration}</p>
+      <p><strong>Score: </strong>${chamberEnsemblesNarr.Score}</p>
+      <p><strong>Part(s): </strong>${chamberEnsemblesNarr.partOrParts}</p>
+      <p><strong>programNoteAvailable: </strong>${chamberEnsemblesNarr.programNoteAvailable}</p>
+      <p><strong>Sheet Music Format: </strong>${chamberEnsemblesNarr.sheetMusicFormat}</p>
+      <p><strong>Recording Format: </strong>${chamberEnsemblesNarr.recordingFormat}</p>
+      <p><strong>URL: </strong><a href="${chamberEnsemblesNarr.URL}" target="_blank">${chamberEnsemblesNarr.URL}</a></p>
+      <p><strong>Catalogue Number: </strong>${chamberEnsemblesNarr.catalogueNo}</p>
+    </div>
+    <hr>
+  `
+  })
+  chamberEnsemblesNarrContainer.innerHTML = chamberEnsemblesNarrHTML;
   
